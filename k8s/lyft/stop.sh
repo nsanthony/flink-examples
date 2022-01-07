bda7cab5b314dd8533f302bf4c1ad841f44f154a
Nathan Anthony <nsanthony@Nathans-MBP.fios-router.home>
2022-01-07 10:54:30 -0500

    deleting the namespace in the removal of the operator

--- k8s/lyft/stop.sh
+++ k8s/lyft/stop.sh
@@ -4,5 +4,5 @@ kubectl delete -f flinkk8soperator.yaml
 kubectl delete -f config.yaml 
 kubectl delete -f role-binding.yaml
 kubectl delete -f role.yaml 
-#kubectl delete -f namespace.yaml 
+kubectl delete -f namespace.yaml 
 kubectl delete -f crd.yaml 
